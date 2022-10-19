f1cf36fee505742c0f8af9a5260d213d46727fff
Nitin011001 <thenitinchauhan01@gmail.com>
2022-10-19 05:11:22 -0400

    added components

---
+++ src/components/Business.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const Business=()=>{
+    return (
+        <div>Billing</div>
+    )
+}
+export default Business
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+const Navbar=()=>{
+    return (
+        <div>Billing</div>
+    )
+}
+export default Navbar
